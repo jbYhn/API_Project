acdfa3f555c5b7ce496a5450ac96ae9399bebec8
Jean-Baptiste Yehouenou <jean-baptiste.yehouenou@epita.fr>
2020-12-07 23:49:58 +0100

    Initial commit : project repositories creation

---
+++ src/test/java/api/project/Api_Project/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package api.project.Api_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
