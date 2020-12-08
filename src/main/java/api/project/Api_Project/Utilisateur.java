7727e759a9a9a8732f5181401208c572258bacff
Jean-Baptiste Yehouenou <jean-baptiste.yehouenou@epita.fr>
2020-12-08 02:05:48 +0100

    Utilisateur class Entity

---
+++ src/main/java/api/project/Api_Project/Utilisateur.java
@@ -0,0 +1,60 @@
+package api.project.Api_Project;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Utilisateur")
+@EntityListeners(AuditingEntityListener.class)
+
+
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column (name  = "email", nullable = false)
+    private String email ;
+
+    @Column( name = "login",  nullable = false)
+    private String login;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+
+}
